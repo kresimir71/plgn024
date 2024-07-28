83b52b2537a8d24b613751f4d206f1d34006cea6
U-LT\Kresimir <Kresimir@LT>
2024-07-28 23:35:09 +0200

    block name and text domain ajusted

--- modules/CopyrightDateBlockModule/react/build/index.asset.php
+++ modules/CopyrightDateBlockModule/react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '78317763cfe110e8c557');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4e9b052df0a08fe8382a');
